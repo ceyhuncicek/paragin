9060c5fb54ca850b776d1f28acd2d0ce8e911783
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-07 17:36:17 +0100

    added colors

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,30 @@
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: 'IndexPage',
+};
 </script>
+<style>
+:root {
+  font-family: Arial, Helvetica, sans-serif;
+  --main-color: #66aa11;
+  --general-error: #cf191e;
+  --text-title: #4a4a4a;
+  --link-color: #3273f6;
+  --white: #ffffff;
+  --box-color: #fafafa;
+  --box-zebra-inside: #f1f1f1;
+  --box-border-outer: #ececec;
+  --box-outer: #f1f1f1;
+}
+
+body {
+  background-color: rgba(0, 0, 0, 0.7);
+}
+</style>
+
+<style scoped>
+.page {
+  margin: 32px;
+}
+</style>
