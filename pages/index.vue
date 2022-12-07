a16b1535cb748a60003408aefa0fc9439c9e4be5
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-07 18:51:02 +0100

    added basic elements to page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="page">
-  <Tutorial/>
+    <Modal title="Add new correction round">
+      <template #content>
+        <p>Modal content</p>
+      </template>
+      <template #secondaryButton>
+        <Button variant="outlined" borderRadius="1x">Cancel</Button>
+      </template>
+      <template #primaryButton>
+        <Button variant="contained" color="danger" borderRadius="1x">
+          <i class="fas fa-exclamation-triangle"></i>
+          Save this correction round
+        </Button>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Button from '../components/forms/atoms/Button';
+import Modal from '../components/modals/molecules/Modal';
 export default {
+  components: { Modal, Button },
   name: 'IndexPage',
 };
 </script>
