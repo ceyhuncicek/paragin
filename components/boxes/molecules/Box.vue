3123f67c46b65ba1334e3ef006684f6b1149ffbf
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-07 19:44:00 +0100

    added box component

---
+++ components/boxes/molecules/Box.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <div class="box__header">{{ title }}</div>
+    <div class="box__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Box',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box__header {
+  color: var(--text-title);
+  font-weight: 400;
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+</style>
