e8e28719e652405cc28643d52b1d44fbcc0deb95
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-07 20:09:52 +0100

    added radio input

---
+++ components/forms/molecules/RadioInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="radio-input">
+    <label>
+      <input
+        type="radio"
+        :checked="isChecked"
+        :value="value"
+        @change="$emit('change', $event.target.value)" />
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    radioState: {
+      default: '',
+    },
+  },
+  model: {
+    prop: 'radioState',
+    event: 'change',
+  },
+  computed: {
+    isChecked() {
+      return this.radioState == this.value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.radio-input {
+  display: inline;
+}
+</style>
